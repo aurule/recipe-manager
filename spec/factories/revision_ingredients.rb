f38d47df012c5a8de6db5517ce79b30d35c5b3d1
Peter Andrews <peter.andrews@modolabs.com>
2018-07-21 13:03:20 -0400

    Add annotations

--- spec/factories/revision_ingredients.rb
+++ spec/factories/revision_ingredients.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: revision_ingredients
+#
+#  id            :integer          not null, primary key
+#  notes         :text
+#  order         :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  ingredient_id :integer
+#  revision_id   :integer
+#
+# Indexes
+#
+#  index_revision_ingredients_on_ingredient_id       (ingredient_id)
+#  index_revision_ingredients_on_revision_and_order  (,order) UNIQUE
+#  index_revision_ingredients_on_revision_id         (revision_id)
+#
+
 FactoryBot.define do
   factory :revision_ingredient do
     revision
